e3ccc195e9bf3986df9f4922849c64a905fd021b
ilya9293 <ilya9293@ukr.net>
2023-02-11 11:06:22 +0200

    add murkup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
-export const App = () => {
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+const App = () => {
+  const [parts, setParts] = useState([
+    { order: 1 },
+    { order: 2 },
+    { order: 3 },
+    { order: 4 },
+  ]);
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <div className="field">
+        <div className="part"></div>
+        <div className="part"></div>
+        <div className="part"></div>
+        <div className="part"></div>
+      </div>
+      <div className="container">
+        <div className="word">Я</div>
+        <div className="word">хочу</div>
+        <div className="word">работать</div>
+        <div className="word">программистом</div>
+      </div>
+    </>
   );
 };
+
+export default App;
